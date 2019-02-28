98607dd005834839067ba77dd8878011d455cf11
Bhaskar Koley <42509000+bhaskarkoley87@users.noreply.github.com>
2019-02-28 20:28:23 +0530

    Committing the code for Service Registry

---
+++ src/test/java/com/hackfse/giveaway/eureka_server/GiveAwayEurekaServer/GiveAwayEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackfse.giveaway.eureka_server.GiveAwayEurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiveAwayEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
